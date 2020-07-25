6f3a1c325a1ca53fac055592e714ab09977a2e78
Luis Cardozo <me@luiscardozo.dev>
2020-07-25 19:01:33 -0400

    feat: Add main.py file, the future central point of this app

---
+++ src/main.py
@@ -0,0 +1,5 @@
+# 1. video => face detection => cropped face img
+# 2.1 cropped face => landmark detection   =>  eyes images
+# 2.2 cropped face => head pose estimation =>  headpose
+# 3. eyes images + headpose => gaze estimation => gaze direction
+# 4. gaze direction => move mouse pointer
