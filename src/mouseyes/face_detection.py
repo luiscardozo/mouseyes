e7102d52bf27a6b41144de1cef2ff50fa31d2916
Luis Cardozo <me@luiscardozo.dev>
2020-07-27 15:39:28 -0400

    feat: Function get_cropped_face() in Face Detection Model
    * function get_cropped_face to get a cropped ndarray from the original image
    * function _save_image() to debug get_cropped_face()
    * return the same array from predict(), being sync or async

--- src/mouseyes/face_detection.py
+++ src/mouseyes/face_detection.py
@@ -1,4 +1,6 @@
 from mouseyes.model import ModelBase
+import numpy as np
+import cv2
 
 DEFAULT_DEVICE="CPU"
 DEFAULT_MODEL=f'models/intel/face-detection-adas-binary-0001/INT1/face-detection-adas-binary-0001.xml'
@@ -15,14 +17,29 @@ class FaceDetectionModel(ModelBase):
     def __init__(self, model_path=DEFAULT_MODEL, device=DEFAULT_DEVICE, extensions=DEFAULT_EXTENSION, transpose=DEFAULT_TRANSPOSE):
         super().__init__(model_path, device=device, extensions=extensions, transpose=transpose)
         print("Input shape:", super().get_input_shape)
-    
 
-if __name__ == '__main__':
-    #A little "inline" test
-    fd = FaceDetectionModel()
-    from mouseyes.input_feeder import InputFeeder
-    TEST_IMAGE = "tests/resources/center.jpg"
-    ifeed = InputFeeder('image', TEST_IMAGE)
-    ifeed.load_data()
-    print(type(ifeed.cap))
-    print(ifeed.cap.shape)
+    def predict(self, image, sync=False, request_id=0):
+        output = super().predict(image, sync, request_id)
+        if sync:
+            output = output['detection_out']
+
+        return output
+    
+    def get_cropped_face(self, original_img, processed_output, save_file=False):
+        """Returns a np with the cropped face, in original image's size"""
+        coords=processed_output[0]
+        print(coords)
+        print(original_img.shape)
+        cropped_face = original_img[coords[1]:coords[3], coords[0]:coords[2]]
+        #cropped_face = original_img
+        #xmin, ymin, xmax, ymax = processed_output[0]
+        #cv2.rectangle(cropped_face, (xmin, ymin), (xmax, ymax), (255, 0, 0), 2)
+        if save_file:
+            self._save_image(cropped_face)
+        return cropped_face
+
+    def _save_image(self, frame):
+        img_path = f'/home/luis/tmp/cropped_face.jpg'    #for debug only. Change to your own dir
+        print("write ", img_path)
+        cv2.imwrite(img_path, frame)
+        return img_path
