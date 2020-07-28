2dba4cd38e0938fe34833731c37a6a511b5ecf93
Luis Cardozo <me@luiscardozo.dev>
2020-07-27 23:12:27 -0400

    feat: Gaze Estimation Model: implemented preprocess() and predict()

--- src/mouseyes/gaze_estimation.py
+++ src/mouseyes/gaze_estimation.py
@@ -1,4 +1,5 @@
-from .model import ModelBase
+from mouseyes.model import ModelBase
+import numpy as np
 
 class GazeEstimationModel(ModelBase):
     # gaze-estimation-adas-0002
@@ -12,3 +13,28 @@ class GazeEstimationModel(ModelBase):
 
     def __init__(self, model_path, device='CPU', extensions=None, transpose=(2,0,1)):
         super().__init__(model_path, device=device, extensions=extensions, transpose=transpose)
+
+    def preprocess_input(self, left_eye, right_eye, head_pose):
+        prep_left = super().preprocess_input(left_eye, required_size=None, input_name="left_eye_image")
+        prep_right = super().preprocess_input(right_eye, required_size=None, input_name="right_eye_image")
+        prep_head_pose = np.array([head_pose])
+        print("=================== pose", prep_head_pose.shape)
+        return (prep_left, prep_right, prep_head_pose)
+
+    def predict(self, left_eye, right_eye, head_pose, sync=False, request_id=0):
+        '''
+        This method is meant for running predictions on the input images.
+        '''
+        input_blob = {
+            "head_pose_angles": head_pose,
+            "left_eye_image": left_eye,
+            "right_eye_image": right_eye
+        }
+
+        if sync:
+            out = self.infer_sync(input_blob)
+            return out[self.output_name]        #normally returns a dict
+        else:
+            self.infer_async(input_blob, request_id)
+            if self.wait(request_id) == 0:
+                return self.get_output(request_id)
